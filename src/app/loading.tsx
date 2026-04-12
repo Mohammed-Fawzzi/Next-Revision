9924bf3bd0afdd82d65e2203fc9b6140acf33259
Mohamed Fawzzi <mohamedfawzzimohamed@email.com>
2026-04-12 23:50:48 +0300

    feat: add loading component with spinner animation

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function loading() {
+    return (
+        <div className="flex items-center justify-center h-full py-10">
+            <div className="w-10 h-10 border-4 border-sky-300 border-t-sky-800 rounded-full animate-spin"></div>
+        </div>
+    )
+}
